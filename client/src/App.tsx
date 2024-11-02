c1dc323ec3f1e41d8ae25ec284e97f3af5b7c8bb
mohammed <medber1997@gmail.com>
2024-11-02 02:51:04 +0100

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import { Header } from "./components/Header";
+
+function App() {
+  return (
+    <div className="font-bold bg-red-400 flex-1 w-full h-full">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
